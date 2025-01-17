0bfddf0b2c5b7aeb283cb4e34cb280da374a24a9
Juliette Suc <juliette.suc@outlook.fr>
2025-01-17 15:34:53 +0100

    Feat : navbar with logo

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router";
+import NavBar from './components/NavBar.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <NavBar/>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -23,8 +23,8 @@ function Detail() {
 
   return (
     
-    <div className="w-full bg-gray-800">
-       <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+    <div className="w-full h-screen pt-3 bg-gray-800">
+       <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12  bg-gray-900 rounded-lg shadow-lg">
        <div className="text-center pb-12">
       <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
         {beer?.name}
